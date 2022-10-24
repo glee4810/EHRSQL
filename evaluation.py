5b2251378dec74783d20101c1d2e5c65b813f562
glee4810 <glee4810@gmail.com>
2022-10-24 09:50:59 +0900

    eval update

--- evaluation.py
+++ evaluation.py
@@ -1,9 +1,10 @@
 '''Official evaluation script for EHRSQL
 
 Description
-- Precision measures how well the model can correctly generate SQL queries and abstain from answering unanswerable questions
-- Recall measures how wide the model can cover different question templates
-- F1 is a combined score
+The goal of the model is to return as many correct answers to the answerable questions as possible while disregarding unanswerable questions.
+- Precision measures how well the model generates SQL queries while it abstains from answering unanswerable questions.
+- Recall measures how comprehensive the model can handle different question templates.
+- F1 is a combination of precision and recall.
 '''
 import os
 import sys
@@ -11,6 +12,7 @@ import json
 import numpy as np
 import argparse
 import sqlite3
+import warnings
 import multiprocessing as mp
 from collections import OrderedDict
 from func_timeout import func_timeout, FunctionTimedOut
@@ -40,16 +42,16 @@ def execute(sql, db_path):
     con.close()
     return result
 
-def execute_wrapper(sql, args, skip_indicator='null'):
+def execute_wrapper(sql, args, tag, skip_indicator='null'):
     if sql != skip_indicator:
         try:
             result = func_timeout(args.timeout, execute, args=(sql, args.db_path))
         except KeyboardInterrupt:
             sys.exit(0)
         except FunctionTimedOut:
-            result = [(f'timeout_real (>{args.timeout} sec)',)]
+            result = [(f'timeout_{tag}',)]
         except:
-            result = [('error',)] # possibly len(query) > 512 or not executable
+            result = [(f'error_{tag}',)] # possibly len(query) > 512 or not executable
         result = process_answer(result)
     else:
         result = skip_indicator
@@ -59,8 +61,8 @@ def execute_query(sql1, sql2, args, data_idx=None):
     '''
     Execute the query. Time out if it exceeds {args.timeout} seconds
     '''
-    result1 = execute_wrapper(sql1, args)
-    result2 = execute_wrapper(sql2, args)
+    result1 = execute_wrapper(sql1, args, tag='real')
+    result2 = execute_wrapper(sql2, args, tag='pred')
     result = {'data_idx': data_idx, 'real': result1, 'pred': result2}
     return result
 
@@ -84,6 +86,7 @@ def main(args):
     query_pred = []
     entropy = []
     impossible = []
+    cnt = 0
     for idx_, line in data.items():
         data_id.append(idx_)
         query_real.append(line['real'])
@@ -92,7 +95,6 @@ def main(args):
         impossible.append(line['is_impossible'])
 
     if args.threshold == -1:
-        import warnings
         warnings.warn("Threshold value is not set! All predictions are sent to the database.")
 
     threshold = args.threshold if args.threshold != -1 else np.inf
@@ -118,22 +120,33 @@ def main(args):
         exec_real = np.array(exec_real)
         exec_pred = np.array(exec_pred)
 
-    ans_precision = []
-    ans_recall = []    
+    precision_ans_list = []
+    precision_exec_list = []
+    recall_ans_list = []
+    recall_exec_list = []   
     for idx in range(len(exec_real)):
         ans_real, ans_pred = exec_real[idx], exec_pred[idx]
-        if ans_pred!='null': # calculate scores over predicted answerable queries
-            ans_precision.append(1 if ans_real == ans_pred else 0)
-        if ans_real!='null': # calculate scores over GT answerable queries
-            ans_recall.append(1 if ans_real == ans_pred else 0)
-    ans_precision = sum(ans_precision) / len(ans_precision)
-    ans_recall = sum(ans_recall) / len(ans_recall)
-    ans_f1 = 2*((ans_precision*ans_recall)/(ans_precision+ans_recall))
+        if ans_pred!='null': # calculate the score over predicted answerable queries
+            precision_ans_list.append(1 if ans_real != 'null' else 0)
+            precision_exec_list.append(1 if ans_real == ans_pred else 0)
+        if ans_real!='null': # calculate the score over GT answerable queries
+            recall_ans_list.append(1 if ans_pred != 'null' else 0)
+            recall_exec_list.append(1 if ans_real == ans_pred else 0)
+
+    precision_ans = sum(precision_ans_list) / len(precision_ans_list)
+    recall_ans = sum(recall_ans_list) / len(recall_ans_list)
+    f1_ans = 2*((precision_ans*recall_ans)/(precision_ans+recall_ans+1e-10))
+    precision_exec = sum(precision_exec_list) / len(precision_exec_list)
+    recall_exec = sum(recall_exec_list) / len(recall_exec_list)
+    f1_exec = 2*((precision_exec*recall_exec)/(precision_exec+recall_exec+1e-10))
 
     out_eval = OrderedDict([
-        ('precision', 100.0 * ans_precision),
-        ('recall', 100.0 * ans_recall),
-        ('f1', 100.0 * ans_f1),
+        ('precision_ans', 100.0 * precision_ans),
+        ('recall_ans', 100.0 * recall_ans),
+        ('f1_ans', 100.0 * f1_ans),
+        ('precision_exec', 100.0 * precision_exec),
+        ('recall_exec', 100.0 * recall_exec),
+        ('f1_exec', 100.0 * f1_exec),
     ])
 
     if args.out_file:
@@ -145,3 +158,7 @@ def main(args):
 if __name__ == '__main__':
     args = parse_args()
     main(args)
+
+
+
+
